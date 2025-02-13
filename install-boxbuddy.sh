c395dd60ac6409715ef33e72eef7c6e564c3e76f
Rohmat Subekti <rohmatsb@gmail.com>
2025-02-14 00:42:48 +0700

    install distrobox terbaru

--- install-boxbuddy.sh
+++ install-boxbuddy.sh
@@ -1,12 +1,22 @@
 # Menginstall paket yang dibutuhkan
 sudo apt update
-sudo apt install podman distrobox flatpak uidmap slirp4netns -y
+sudo apt install podman flatpak uidmap slirp4netns -y
 
 # Menambahkan repo flathub
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Bersihkan layar
+clear
+
+# Install Distrobox
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
 # Bersihkan layar 
 clear
 
 # Install BoxBuddy
 flatpak install flathub io.github.dvlv.boxbuddyrs -y
+
+# Pesan
+clear
+echo "Instalasi Boxbuddy sudah selesai..."

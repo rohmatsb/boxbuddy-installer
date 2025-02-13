21263c2dfb71c269042c4f11503ea18d2c6a9f6b
Rohmat Subekti <rohmatsb@gmail.com>
2025-02-13 15:28:55 +0000

    system wide install

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # boxbuddy-installer
 
 wget https://raw.githubusercontent.com/rohmatsb/boxbuddy-installer/main/install-boxbuddy.sh && chmod +x install-boxbuddy.sh && ./install-boxbuddy.sh
-
-Pengingat untuk diri sendiri :
-(jalankan dengan command diatas, jangan jalankan menggunakan 'sudo' seperti ini 'sudo ./install-boxbuddy.sh')
--- install-boxbuddy.sh
+++ install-boxbuddy.sh
@@ -3,10 +3,10 @@ sudo apt update
 sudo apt install podman distrobox flatpak uidmap slirp4netns -y
 
 # Menambahkan repo flathub
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Bersihkan layar 
 clear
 
 # Install BoxBuddy
-flatpak install --user flathub io.github.dvlv.boxbuddyrs -y
+flatpak install flathub io.github.dvlv.boxbuddyrs -y
